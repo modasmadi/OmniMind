965e468c96f466c27ddbf975c79d519edfd8cb18
modasmadi <mahmoudsmadi30@gmail.com>
2025-12-28 05:18:33 +0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import OmniInterface from '@/components/OmniInterface';
-
-export default function Home() {
-    return (
-        <main className="min-h-screen bg-black flex items-center justify-center">
-            <OmniInterface />
-        </main>
-    );
-}
